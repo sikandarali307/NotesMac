0493ab2ec6203354014c16403662039fa9a564b2
sikandarali307 <sikandar.ali307@gmail.com>
2021-10-25 11:03:43 +0500

    Initial Commit

---
+++ Shared/NotesMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesMacApp.swift
+//  Shared
+//
+//  Created by Mac on 25/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
